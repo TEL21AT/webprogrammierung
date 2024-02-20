04a7f12442345119576d751de2881ab3ced83d41
Daniel Muench <d.muench@sap.com>
2024-02-20 08:09:00 +0100

    update

--- Vorlesungsmaterial/nodejs/Vanilla_Backend/index_full.js
+++ Vorlesungsmaterial/nodejs/Vanilla_Backend/index_full.js
@@ -1,15 +1,19 @@
 const http = require('node:http');
 const { URL } = require('url');
 const { parse } = require('querystring');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 const origin = `http://${hostname}:${port}`;
+
 const server = http.createServer((req, res) => {
+
   const url = new URL(req.url, origin);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n' + parse(url.search.substring(1)).test);
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at ${origin}`);
 });
